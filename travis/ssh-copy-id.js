faa52a6e15da3e252648431fc0e0349868c69d6a
clement <maboiteaspam@wanadoo.fr>
2014-09-06 19:53:04 +0800

    fix travis file

--- travis/ssh-copy-id.js
+++ travis/ssh-copy-id.js
@@ -5,20 +5,44 @@ var spawn = require('child_process').spawn;
 
 var ls    = spawn('ssh-copy-id', ['travis@localhost']);
 
-var t_out = setTimeout(function(){
-  console.log('stdin: \n');
-  ls.stdin.write("\n")
-},1000);
+var t_out = null;
+var w_lngt = 0;
+var s_out = "";
+var start_listen = function(){
+  if(!t_out){
+    t_out = setInterval(function(){
+      if( w_lngt > 0 ){
+        try{
+          ls.stdin.write("\n")
+          console.log('writing stdin');
+        }catch(ex){
+
+        }
+      }
+      w_lngt++;
+    },1000);
+  }
+}
 
 ls.stdout.on('data', function (data) {
+  if( s_out != data ){
+    s_out = data;
+    w_lngt = 0;
+  }
   console.log('stdout: ' + data);
+  start_listen();
 });
 
 ls.stderr.on('data', function (data) {
+  if( s_out != data ){
+    s_out = data;
+    w_lngt = 0;
+  }
   console.log('stderr: ' + data);
+  start_listen();
 });
 
 ls.on('close', function (code) {
-  clearTimeout(t_out);
+  clearInterval(t_out);
   console.log('child process exited with code ' + code);
 });
