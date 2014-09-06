b1d51bd953474da9bf2372e70d3d6510a48c61ca
clement <maboiteaspam@wanadoo.fr>
2014-09-06 19:56:35 +0800

    fix travis file

--- travis/install-openssh-server.js
+++ travis/install-openssh-server.js
@@ -5,44 +5,17 @@ var spawn = require('child_process').spawn;
 
 var ls    = spawn('sudo', ['apt-get', 'install', 'openssh-server']);
 
-var t_out = null;
-var w_lngt = 0;
-var s_out = "";
-var start_listen = function(){
-  if(!t_out){
-    t_out = setInterval(function(){
-      if( w_lngt > 0 ){
-        try{
-          ls.stdin.write("\n")
-          console.log('writing stdin');
-        }catch(ex){
-
-        }
-      }
-      w_lngt++;
-    },1000);
-  }
-}
-
 ls.stdout.on('data', function (data) {
-  if( s_out != data ){
-    s_out = data;
-    w_lngt = 0;
-  }
   console.log('stdout: ' + data);
-  start_listen();
+  if( data.toString().match(/Enter file in which to save the key/) ){
+    ls.stdin.write("\n")
+  }
 });
 
 ls.stderr.on('data', function (data) {
-  if( s_out != data ){
-    s_out = data;
-    w_lngt = 0;
-  }
   console.log('stderr: ' + data);
-  start_listen();
 });
 
 ls.on('close', function (code) {
-  clearInterval(t_out);
   console.log('child process exited with code ' + code);
 });
--- travis/ssh-keygen.js
+++ travis/ssh-keygen.js
@@ -5,44 +5,20 @@ var spawn = require('child_process').spawn;
 
 var ls    = spawn('ssh-keygen', ['-t', 'dsa']);
 
-var t_out = null;
-var w_lngt = 0;
-var s_out = "";
-var start_listen = function(){
-  if(!t_out){
-    t_out = setInterval(function(){
-      if( w_lngt > 0 ){
-        try{
-          ls.stdin.write("\n")
-          console.log('writing stdin');
-        }catch(ex){
-
-        }
-      }
-      w_lngt++;
-    },1000);
-  }
-}
-
 ls.stdout.on('data', function (data) {
-  if( s_out != data ){
-    s_out = data;
-    w_lngt = 0;
-  }
   console.log('stdout: ' + data);
-  start_listen();
 });
 
 ls.stderr.on('data', function (data) {
-  if( s_out != data ){
-    s_out = data;
-    w_lngt = 0;
-  }
   console.log('stderr: ' + data);
-  start_listen();
+  if( data.toString().match(/Enter file in which to save the key/) ){
+    ls.stdin.write("\n")
+    setTimeout(function(){
+      ls.stdin.write("\n")
+    },1000);
+  }
 });
 
 ls.on('close', function (code) {
-  clearInterval(t_out);
   console.log('child process exited with code ' + code);
 });
