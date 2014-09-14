19d575ef102b6c6b7a3b391cc67d603ebe230d0f
clement <maboiteaspam@wanadoo.fr>
2014-09-14 15:29:57 +0800

    update travis file

--- test/available_host.js
+++ test/available_host.js
@@ -18,10 +18,6 @@ describe('grunt-sftp-deploy nicely fails to unavailable hosts', function () {
 
     before(function(){
         grunt.file.mkdir("test/fixture/src")
-        grunt.file.mkdir("test/fixture/dest")
-        console.log(
-            grunt.file.expand("./**")
-        )
     });
 
     after(function(){
