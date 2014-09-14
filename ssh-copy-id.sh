ecbcb566e1ed9cf5b449a29b8b3ae710be0c3250
clement <maboiteaspam@wanadoo.fr>
2014-09-14 12:11:40 +0800

    update travis file

--- ssh-copy-id.sh
+++ ssh-copy-id.sh
@@ -2,6 +2,6 @@
 spawn ssh-copy-id $argv
 expect "password:"
 send "\r"
-expect "Are you sure you want to continue connecting (yes/no)? "
+expect "Are you sure you want to continue connecting (yes/no)?"
 send "\r"
 expect eof
